42a7736320c8e065cebe4386439d072a061e6d1f
chenguijian <chenguijian@starmerx.com>
2023-11-29 16:08:25 +0800

    完善代码

--- MyApplication/app/src/main/java/com/example/myapplication/keep_app_live/reset_rule/ResetRule.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/keep_app_live/reset_rule/ResetRule.kt
@@ -9,17 +9,23 @@ import java.util.Calendar
 object ResetRule {
 
     fun handleLogic(context : Context){
+        if(getLastExecuteTime(context) <= 0){
+            //设置上一个执行的时间
+            setLastExecuteTime(context, Calendar.getInstance().timeInMillis)
+            return
+        }
         val timeDiff = Calendar.getInstance().timeInMillis - getLastExecuteTime(context)
-        if (timeDiff >= 3*60*1000) {
+        if (timeDiff <= 30*1000 || timeDiff >= 3*60*1000) {
             Log.e("ATU", "Worker 杀死所有定时Work重新来")
             keepAppLive(context)
+            return
         }
         //设置上一个执行的时间
-        setLastExecuteTime(context)
+        setLastExecuteTime(context, Calendar.getInstance().timeInMillis)
     }
 
     fun handleInit(context : Context){
-        setLastExecuteTime(context)
+        setLastExecuteTime(context, 0)
     }
 
     const val KEY_LAST_EXECUTE_TIME = "KEY_LAST_EXECUTE_TIME"
@@ -29,9 +35,9 @@ object ResetRule {
         ).getLong(KEY_LAST_EXECUTE_TIME, Calendar.getInstance().timeInMillis)
     }
 
-    private fun  setLastExecuteTime(context: Context){
+    private fun  setLastExecuteTime(context: Context, time:Long){
         SPUtils.getInstance(
             context
-        ).put(KEY_LAST_EXECUTE_TIME, Calendar.getInstance().timeInMillis)
+        ).put(KEY_LAST_EXECUTE_TIME, time)
     }
 }
